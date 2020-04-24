de7993980babc6e318e90bbd73ddaa93ffce2d55
dpthegrey <dpthegrey@gmail.com>
2020-04-25 01:29:21 +0530

    initial commit

---
+++ my_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/dpthegrey/PROJECTS/django3-By-Example/my_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Django3_Blog_Application
+This is a result from tutorial.
+I am learning how to make full django app from the Django 3 By Example - Third Edition book.
